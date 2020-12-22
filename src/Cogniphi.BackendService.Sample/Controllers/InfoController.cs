99d4076c0f880238e83378b867a83445ff577770
vimalkrishnan <vimal_viyookaran@yahoo.co.in>
2020-12-22 09:08:34 +0530

    solution corrected

---
+++ src/Cogniphi.BackendService.Sample/Controllers/InfoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cogniphi.BackendService.Sample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "sample backend";
+        }
+    }
+}
